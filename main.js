1b5eebb94e7b0ba80767634b257204c098ef5d81
ErnstAnton <ernst.schindler@student.uibk.ac.at>
2024-03-21 17:48:53 +0100

    karte und popup fertig

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var map = L.map('map').setView([-41.293056, 174.001944], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([-41.293056, 174.001944]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
