6ee25bfa2e5bce443b05e0c64fc2631e48fbcda7
ErnstAnton <ernst.schindler@student.uibk.ac.at>
2024-04-11 16:15:25 +0200

    lat/lng mit fünf nachkommastellen

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+let lat = -41.293056;
+let lng = 174.001944;
+let zoom = 13;
 
-let map = L.map('map').setView([-41.293056, 174.001944], 13);
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-41.293056, 174.001944]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+let marker = L.marker([lat, lng]).addTo(map);
+marker.bindPopup(`
+<h2>Picton<h2>
+<ul>
+<li>Breite: ${lat.toFixed(5)} </li>
+<li>Länge: ${lng.toFixed(5)} </li>
+</ul>
+`).openPopup();
 
 
 
