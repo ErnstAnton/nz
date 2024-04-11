816182442f64164c30c0b639a3d23848088d910b
ErnstAnton <ernst.schindler@student.uibk.ac.at>
2024-04-11 15:55:52 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
-var map = L.map('map').setView([-41.293056, 174.001944], 13);
+let map = L.map('map').setView([-41.293056, 174.001944], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-41.293056, 174.001944]).addTo(map);
+let marker = L.marker([-41.293056, 174.001944]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+
 
